e92469266e49744fca4b78b2c00a73290d9eb683
Rian Aquino <rian.aquino@outlook.com>
2022-11-03 14:45:42 -0300

    🔧 chore(tailwind): setup tailwind and postcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import './styles/global.css';
+
 export function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="bg-[#121214] w-screen h-screen text-white flex items-center justify-center">
+      <h1 className="text-2xl font-black">Hello World!</h1>
+    </div>
+  );
 }
