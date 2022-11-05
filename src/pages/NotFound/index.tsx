46e29d09b9f8f81e5dd50b1eab35b5c0499fc50e
Rian Aquino <rian.aquino@outlook.com>
2022-11-05 02:49:56 -0300

    🔄 refactor(theming): update base pages to new theming system

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 export function NotFound() {
   return (
-    <div className="bg-indigo-900 w-screen h-screen flex flex-col gap-4 items-center justify-center">
-      <h1 className="text-white text-3xl font-black">Página não encontrada :/</h1>
-      <Link to="/" className="bg-slate-900 px-8 py-2 rounded text-white">
+    <div className="bg-dominant-200 w-screen h-screen flex flex-col gap-4 items-center justify-center">
+      <h1 className="text-contrast-200 text-3xl font-black">Página não encontrada :/</h1>
+      <Link to="/" className="bg-primary-500 px-8 py-2 rounded text-gray-light-100">
         Ir para a Página Inicial
       </Link>
     </div>
