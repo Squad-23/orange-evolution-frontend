80993c6f9deb90665e83b1ec78aa386854bdb4d9
majhara <may.simoes@hotmail.com>
2022-11-06 16:32:59 -0300

    💄 style(index.tsx): styling login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -5,22 +5,22 @@ export function Login() {
 
 
   return (
-    <div className="bg-gradient-to-t from-[#ff3200] to-[#ffa000] w-screen h-screen flex flex-col gap-4 items-center justify-center">
-      <h1 className="text-white text-5xl font-black">Orange Evolution </h1>
+    <div className="bg-zinc-900 w-screen h-screen flex flex-col gap-4 items-center justify-center">
+      <h1 className="text-[#ffa000] text-5xl font-extrabold">Orange Evolution </h1>
         <div className="m-5 p-5 box-border">
           <div className="h-full">
-            <div className="bg- backdrop-blur w-max rounded-3xl overflow-hidden p-77-55-33-95 shadow-xl">
+            <div className="bg-gradient-to-br from-[#ff5a23] to-[#ffa000] backdrop-blur-3xl  w-max rounded-3xl overflow-hidden p-77-55-33-95 shadow-xl">
               
               <form className="w-full p-10">
                 <span className="text-3xl font-black text-white flex center justify-center mb-10">Bem-vindo </span>
                 
                 <div className="w-full relative border-orange-700 bottom-2 border-solid mb-3">
                   <input 
-                    className="bg-white mt-8 top-5 rounded-sm ml-3"
+                    className="bg-white justify-center top-5 rounded-md ml-3"
                     type="email"
                     
                     />
-                    <span className="absolute block w-full h-full top-0 left-0 pointer-events-none" data-placeholder="Email">Email </span>
+                    <span className="absolute block w-full h-full top-0 left-0 text-lg pointer-events-none" data-placeholder="Email">Email </span>
               </div>
 
               <div className="w-full relative mb-3">
@@ -29,7 +29,7 @@ export function Login() {
                   type="password"
                   
                   />
-                <span className="absolute block top-0 left-0 pointer-events-none" data-placeholder="Password"> Senha </span>
+                <span className="absolute block w-full h-full top-0 left-0 pointer-events-none" data-placeholder="Password"> Senha </span>
               </div>
               
               <div className="flex justify-center align-center pb-13">
