f8efe439c829bbb7d7be3b9223c360b8a18f1372
majhara <may.simoes@hotmail.com>
2022-11-10 01:53:34 -0300

    ✨ feat(dashboard/index.tsx): styling dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -10,15 +10,17 @@ export function Dashboard(){
         </div>
         <div className="bg-gradient-to-r from-[#FD7AC54D] to-[#267CBC26] w-screen h-[500px]">
           <div className="mx-16 my-16 flex ">
-            <div>
-              <h1 className="text-[#00C19C] text-5xl font-semibold ">Bem vinda, fulana</h1>
-                <div className="flex  mr-32 h-72 w-auto">
-                  <p className="font-medium text-2xl">Escolha uma ou mais trilhas para seguir...</p>
-                  <p className="font-medium text-xl"> Ao começar uma trilha você consegue acompanhar a sua evolução conforme consome os conteúdos!</p>
+            <div className="flex flex-col content-between w-1/2">
+              <h1 className="text-[#00C19C] text-5xl font-semibold pb-7 ">Bem vinda, Maria Angélica</h1>
+                <div className="flex-col text-[#333333]">
+                  <p className="font-medium text-2xl pb-10">Escolha uma ou mais trilhas para seguir...</p>
+                  <p className="font-medium text-xl pb-5"> Ao começar uma trilha você consegue acompanhar a sua evolução conforme consome os conteúdos!</p>
                   <p className="font-medium text-xl">Por meio das trilhas você planta a <strong>semente do seu conhecimento</strong> e, assim como uma laranjeira, futuramente você colherá os frutos dos seus estudos. Vamos nessa?</p>
                 </div>
             </div>
-            <img src={ TrilhaLaranja } alt="" className="flex-shrink-0"/>
+            <div className="pl-36 w-auto">
+              <img src={ TrilhaLaranja } alt=""/>
+            </div>
           </div>
         </div>
       </div>
