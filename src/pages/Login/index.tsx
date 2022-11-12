a29ee5026ea8508407e0a414effb4a30e91224af
majhara <may.simoes@hotmail.com>
2022-11-12 14:46:48 -0300

    ✨ feat(login/index-tsx): login page almost ready
    styled login page - missing input hover, font, button hover and change dark mode gradient

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -15,16 +15,24 @@ export function Login() {
       
 
       <div className="flex mx-[31.8125rem] bg-dominant-100 h-[34.375rem] rounded-2xl py-10 box-border shadow-[10px_13px_8px_rgba(0,0,0,0.2)]">
-      <form action="" className="flex flex-col items-center px-6 ">
-        <img src={OrangeLogo} alt="" className="flex w-[7.5rem] h-[7.5rem] mb-8"/>
-          <p className="mt-4 text-contrast-200 font-medium">Email</p>
-        <input type="text" className="bg-dominant-200 rounded-[0.25rem] border border-dominant-300 py-2 px-2 focus:outline-none" placeholder='user@gmail.com'/>
-          <p className="text-contrast-200 font-medium">Password</p>
-        <input type="password" className="bg-dominant-200 rounded-[0.25rem] text-contrast-200 border border-dominant-300 py-2 px-2 focus:outline-none" placeholder='password'/>
-        <a href="" className="mt-4">Esqueci minha senha</a>
-        <button type="button" className="w-[22.375rem] h-12 bg-[#00C19C] rounded-2xl mt-4 text-xl text-gray-50 font-semibold">Fazer login</button>
-        
-      </form>
+        <form action="" className="flex flex-col items-center px-8 ">
+          
+          <img src={OrangeLogo} alt="" className="flex w-[7.5rem] h-[7.5rem] mb-8"/>
+            <div className="pb-4"> 
+              <span className="text-contrast-200 font-medium ">Email</span>
+              <input type='email' className="bg-dominant-200 rounded-[0.25rem] w-[22.375rem] border border-gray-400 py-2 px-2 focus:outline-none" placeholder='user@gmail.com'/>
+            </div>
+
+            <div>
+              <span className="text-contrast-200 font-medium">Password</span>
+              <input type='password' className="bg-dominant-200 rounded-[0.25rem] w-[22.375rem] border border-gray-400 text-contrast-200 py-2 px-2 focus:outline-none"/>
+            </div>
+          
+            <a href="" className="mt-4">Esqueci minha senha</a>
+            
+            <button type="button" className="w-[22.375rem] h-12 bg-[#00C19C] rounded-2xl mt-4 text-xl text-gray-50 font-semibold">Fazer login</button>
+          
+        </form>
       </div>
       </div>
       
