b93f0935bd2bd7ac3eb11dbb6d4680cc1e18a59d
majhara <may.simoes@hotmail.com>
2022-11-12 22:58:46 -0300

    💄 style(login/index.tsx): completed styled login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -7,35 +7,41 @@ import { ThemeSwitcher } from '../../components/ThemeSwitch';
 
 export function Login() {
   return (
-    <body className="bg-dominant-100 w-screen h-screen">
+    <div className="bg-dominant-100 w-screen h-screen">
       <div className="bg-gradient-to-r from-[#FD7AC54D] to-[#D5FFAB80] w-screen h-screen f">
       <span className="flex flex-row-reverse pr-40 pt-10 pb-8 shadow-">
-        <ThemeSwitcher/>
+      <ThemeSwitcher/>
       </span>
       
 
       <div className="flex mx-[31.8125rem] bg-dominant-100 h-[34.375rem] rounded-2xl py-10 box-border shadow-[10px_13px_8px_rgba(0,0,0,0.2)]">
-        <form action="" className="flex flex-col items-center px-8 ">
+        
+        <form action="" className="flex flex-col px-8 ">
           
-          <img src={OrangeLogo} alt="" className="flex w-[7.5rem] h-[7.5rem] mb-8"/>
+          <span className="">
+            <img src={ OrangeLogo } alt="Logo Orange Evolution" className="flex w-[7.5rem] h-[7.5rem] mx-32 mb-8"/>
+          </span>
+
             <div className="pb-4"> 
-              <span className="text-contrast-200 font-medium ">Email</span>
-              <input type='email' className="bg-dominant-200 rounded-[0.25rem] w-[22.375rem] border border-gray-400 py-2 px-2 focus:outline-none" placeholder='user@gmail.com'/>
+              <span className="text-contrast-200 text-base font-semibold">Email</span>
+              <input type='email' className="mt-2 bg-dominant-200 rounded-[0.25rem] w-[22.375rem] border border-gray-400 py-2 px-2 focus:outline-none" placeholder="user@email.com"/>
             </div>
 
             <div>
-              <span className="text-contrast-200 font-medium">Password</span>
-              <input type='password' className="bg-dominant-200 rounded-[0.25rem] w-[22.375rem] border border-gray-400 text-contrast-200 py-2 px-2 focus:outline-none"/>
+              <span className="text-contrast-200 font-semibold">Password</span>
+              <input type='password' className="mt-2 bg-dominant-200 rounded-[0.25rem] w-[22.375rem] border border-gray-400 text-contrast-200 py-2 px-2 focus:outline-none" placeholder="•••••••••••"/>
             </div>
           
-            <a href="" className="mt-4">Esqueci minha senha</a>
             
-            <button type="button" className="w-[22.375rem] h-12 bg-[#00C19C] rounded-2xl mt-4 text-xl text-gray-50 font-semibold">Fazer login</button>
+              <button type="button" className="underline text-[#267CBC] text-left my-6 align-center"> Esqueci minha senha</button>
+
+            
+            <button type="button" className="w-[22.375rem] h-12 bg-[#00C19C] rounded-2xl text-xl text-gray-50 font-semibold">Fazer login</button>
           
         </form>
       </div>
       </div>
       
-    </body>
+    </div>
   );
 }
