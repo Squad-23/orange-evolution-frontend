dd07da7d77bea2977221583d8906595a392c881d
Rian Aquino <rian.aquino@outlook.com>
2022-11-13 17:42:04 -0300

    ✨ feat: create card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,23 +1,52 @@
+import { Link } from 'react-router-dom';
+
+import { convertMinutesToHours } from '../utils/convertMinutesToHours';
+
 export interface CardProps {
-    title: string;
-    banner: string;
-    duration: number;
-    progress: number;
-    description: string;
+  title: string;
+  banner: string;
+  duration: number;
+  progress: number;
+  description: string;
 }
 
-export function Card({ title, banner, description, progress, duration }: CardProps){
-    
+export function Card({ title, banner, description, progress, duration }: CardProps) {
+  return (
+    <Link
+      to={`/trails/${title}`}
+      className="
+        flex-1 rounded-2xl overflow-clip
+        bg-gray-light-300 dark:bg-gray-dark-100 text-contrast-200 shadow-md
+        flex flex-col
 
-    return(
-    <div className="w-[26.5rem] h-[38.625rem] bg-dominant-100 rounded-2xl flex overflow-clip">
-        <div className="flex flex-col">
-            <img src={ banner } alt={`${title} banner`} className="w-full"/>
-            <h3 className="text-2xl color font-semibold pt-5 px-6"> { title } </h3>
-            <span className="pt-5 px-6 text-base font-medium">Tempo Estimado: { duration }</span>
-            <p className="text-xl flex justify-center py-5">{ progress }</p>
-            <p className="text-xl left px-5">{ description }</p>   
+        transition-transform cursor-pointer
+        hover:-translate-y-4 hover:shadow-2xl
+        focus:-translate-y-4 focus:shadow-2xl
+      "
+    >
+      <img src={banner} alt={`${title} banner`} className="w-full h-44 object-cover" />
+
+      <div className="p-6 flex flex-col gap-5">
+        <h3 className="text-2xl color font-semibold"> {title} </h3>
+        <span className="text-base font-medium">Tempo Estimado: {convertMinutesToHours(duration)}</span>
+
+        <div
+          aria-label="Seu progresso na trilha"
+          className="w-full h-8 bg-white bg-opacity-30 rounded-full overflow-clip relative"
+        >
+          <span className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">{progress}%</span>
+          <div
+            className="
+              h-full bg-brand-orange-400
+              text-base font-medium text-blue-100 text-center
+              p-0.5 leading-none
+            "
+            style={{ width: `${progress}%` }}
+          />
         </div>
-    </div>    
-)
+
+        <p className="text-xl">{description}</p>
+      </div>
+    </Link>
+  );
 }
