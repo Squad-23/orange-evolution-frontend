d1e96b003ff1edea71c48f7cac48b4455e49ae3b
majhara <may.simoes@hotmail.com>
2022-11-13 11:40:17 -0300

    💄 style(login/index.tsx): working on responsivity

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -8,13 +8,13 @@ import { ThemeSwitcher } from '../../components/ThemeSwitch';
 export function Login() {
   return (
     <div className="bg-dominant-100 w-screen h-screen">
-      <div className="bg-gradient-to-r from-[#FD7AC54D] to-[#D5FFAB80] w-screen h-screen f">
-      <span className="flex flex-row-reverse pr-40 pt-10 pb-8 shadow-">
+      <div className="bg-gradient-to-r from-[#FD7AC54D] to-[#D5FFAB80] w-screen h-screen">
+      <span className="flex flex-row-reverse pr-40 pt-10 pb-8">
       <ThemeSwitcher/>
       </span>
       
 
-      <div className="flex mx-[31.8125rem] bg-dominant-100 h-[34.375rem] rounded-2xl py-10 box-border shadow-[10px_13px_8px_rgba(0,0,0,0.2)]">
+      <div className="flex mx-[31.8125rem] bg-dominant-100 absolute h-[34.375rem] rounded-2xl py-10 box-border shadow-[10px_13px_8px_rgba(0,0,0,0.2)]">
         
         <form action="" className="flex flex-col px-8 ">
           
@@ -23,13 +23,13 @@ export function Login() {
           </span>
 
             <div className="pb-4"> 
-              <span className="text-contrast-200 text-base font-semibold">Email</span>
-              <input type='email' className="mt-2 bg-dominant-200 rounded-[0.25rem] w-[22.375rem] border border-gray-400 py-2 px-2 focus:outline-none" placeholder="user@email.com"/>
+              <span className="text-contrast-200 sm:text-base md: text-base lg:text-base font-semibold">Email</span>
+              <input type='email' className="mt-2 bg-dominant-200 rounded-[0.25rem] w-[22.375rem] border-gray-400 border-[2px] py-2 px-2 focus:outline-none" placeholder="user@email.com"/>
             </div>
 
             <div>
               <span className="text-contrast-200 font-semibold">Password</span>
-              <input type='password' className="mt-2 bg-dominant-200 rounded-[0.25rem] w-[22.375rem] border border-gray-400 text-contrast-200 py-2 px-2 focus:outline-none" placeholder="•••••••••••"/>
+              <input type='password' className="mt-2 bg-dominant-200 rounded-[0.25rem] w-[22.375rem] border-[2px] border-gray-400 text-contrast-200 py-2 px-2 focus:outline-none" placeholder="•••••••••••"/>
             </div>
           
             
