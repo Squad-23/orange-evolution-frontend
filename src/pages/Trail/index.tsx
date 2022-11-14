e0acdd82a161a79bfa335ffcf53179697d8192dc
majhara <may.simoes@hotmail.com>
2022-11-14 00:29:11 -0300

    ✨ feat(trail/index.tsx): styling trail page

--- src/pages/Trail/index.tsx
+++ src/pages/Trail/index.tsx
@@ -1,31 +1,53 @@
 import TrilhaLaranja from '../../assets/imagem-trilha-laranja.svg'
-import { Footer } from '../../components/Footer';
 import { Menu } from '../../components/Menu';
-
-
+import * as Accordion from '@radix-ui/react-accordion';
+import { ChevronDownIcon } from '@radix-ui/react-icons';
 
 export function Trail(){
     return (
-        <div className="bg-dominant-100 w-screen h-screen bg-gradient-to-r from-[#FD7AC54D] to-[#267CBC26]">
-            <Menu/>
-                <div className="pt-7 px-16 flex from ">
-                    <div className="flex flex-col   space-y-3">
-                        <span className="flex flex-col">
-                            <h3 className="text-xl font-">VOCÊ ESTÁ NA TRILHA DE UX/UI DESIGN </h3>
-                            <h1 className="font-semibold text-[40px] text-brand-green">Boa noite, Maria Angélica</h1>
-                            <p className="text-left italic pr-24">Você sabia que o tempo médio para uma laranjeira dar frutos é de 24 meses após o plantio?</p>
-                        </span>
-                            <img src={ TrilhaLaranja } alt="Imagem de evolução pé de laranja" className="pt-14 pb-40" />
-                        <span className="flex flex-col  ">
-                            <p className="text-4xl font-medium pb-16">Continue de onde você parou...</p>
-                            <div>
-                                <p className="text-xl font-medium pb-7">O INÍCIO</p>
-                                <h1 className="text-5xl font-semibold text-[#861CD2] pb-36">Migração de carreira</h1>
-                            </div>
-                        </span>
-                    </div>
+        <div className="bg-dominant-100 bg-gradient-to-r from-[#FD7AC54D] to-[#D5FFAB80] flex flex-col dark:bg-gradient-to-l w-screen h-screen">
+           
+            <div className="w-screen">
+                <Menu/>
+            </div>
+
+            <div className="flex flex-col px-16 mt-40">
+                <div className="">
+                    <h2 className="text-xl font-normal pb-7">VOCÊ ESTÁ NA TRILHA DE UX/UI DESIGN </h2>
+                    <p className="text-xl italic font-medium">Você sabia que o tempo médio para uma laranjeira dar frutos é de 24 meses após o plantio?</p>
                 </div>
-            <Footer/>
+
+                <img src={ TrilhaLaranja } alt="" className="h-40 w-[28.75rem]"/>
+            </div>
+
+
+            
+            <div className="px-16 w-80">
+                <input type="search" className="w-screen h-20 bg-dominant-200 rounded-lg border-[2px] border-contrast-100 overflow-hidden pl-16" placeholder='Busque por uma aula ou assunto'/>
+                
+                <div className="bg-dominant-200 border-[2px] border-contrast-100 w-screen pl-16 py-5">
+                    <p className="font-normal text-sm pb-2">MÓDULO</p>
+                    <p className="font-medium text-xl">O início</p>
+                </div>
+
+                <div className="bg-dominant-200 border-[2px] border-contrast-100 w-screen pl-16 py-5">
+                    <p className="font-normal text-sm pb-2">MÓDULO</p>
+                    <p className="font-medium text-xl">Fundamentos de UX</p>
+                </div>
+                
+                <div className="bg-dominant-200 border-[2px] border-contrast-100 w-screen pl-16 py-5">
+                    <p className="font-normal text-sm pb-2">MÓDULO</p>
+                    <p className="font-medium text-xl">Fundamentos de UI</p>
+                </div>
+
+                <div className="bg-dominant-200 border-[2px] border-contrast-100 w-screen pl-16 py-5 rounded-lg">
+                    <p className="font-normal text-sm pb-2">MÓDULO </p>
+                    <p className="font-medium text-xl">Opcional</p>
+                </div>
+            </div>
+
+
+
         </div>
 )
 }
