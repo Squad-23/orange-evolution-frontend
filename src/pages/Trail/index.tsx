1d6ee356b5a1fda2837587499a921744a26cf6f1
majhara <may.simoes@hotmail.com>
2022-11-14 17:06:45 -0300

    ✨ feat(trail/index.tsx): working on responsive design of trail page

--- src/pages/Trail/index.tsx
+++ src/pages/Trail/index.tsx
@@ -1,10 +1,5 @@
 import EvolucaoLaranja from '../../assets/images/evolucao-laranja.png'
 import { Menu } from '../../components/Menu';
-import { CircularProgress } from '../../components/CircularProgress';
-import Accordion from '@radix-ui/react-accordion';
-
-
-
 
 export function Trail(){
     return (
@@ -14,19 +9,18 @@ export function Trail(){
             
             <div className="w-full h-full bg-gradient-to-r from-[#FD7AC54D] to-[#D5FFAB80] pt-7 px-6">
             
-            <section className="mt-24">
-                <h3 className="text-base font-medium bg-contrast pb-4 text-contrast-300 md:text-xl "> VOCÊ ESTÁ NA TRILHA DE UX/UI DESIGN </h3>
-                <p className="text-sm italic font-normal text-contrast-300 sm:text-xl"> Você sabia que o tempo médio para uma laranjeira dar frutos é de 24 meses após o plantio?</p>
-
-            <img src={ EvolucaoLaranja } alt="" />
-
-            </section>
+                <section className="mt-24 flex flex-col md:flex-row sm:ml-16">
+                    <div className="">
+                        <h3 className="text-base font-medium bg-contrast my-4 text-contrast-300 sm:text-xl "> VOCÊ ESTÁ NA TRILHA DE UX/UI DESIGN </h3>
+                        <p className="text-sm italic font-normal my-4 text-contrast-300 sm:text-xl"> Você sabia que o tempo médio para uma laranjeira dar frutos é de 24 meses após o plantio?</p>
+                    </div>
+                
+                    <div className="sm:ml-24 sm:mr-16 ">
+                        <img src={ EvolucaoLaranja } alt="" className="w-80 h-28 sm:w-[28.75rem] sm:h-40" />
+                    </div>
+                </section>
             
             </div>
-
-   
-
-
         </div>
 )
 }
